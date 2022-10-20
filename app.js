0287143a42e81cbf1c46504b9ccc391f80c7c1af
jaba777 <tskhovreba777@gmail.com>
2022-10-20 20:16:05 +0400

    add icons

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 
 function initMap(){
 
+  let boolean=false;
+
   var options={
     center: {lat: 41.7151, lng: 44.8271},
     zoom: 12,
@@ -26,26 +28,69 @@ function initMap(){
 
     //add Marker
 
+
+    
+
+    const svgIcon={
+      url: '/img/location.png',
+     // size: new google.maps.Size(17,17),
+     // origin: new google.maps.Point(10, 10),
+      anchor: new google.maps.Point(17, 54),
+      scaledSize: new google.maps.Size(35, 35),
+    }
+
+    const nextIcons=[
+      {
+        url: '/img/1024px-Saburtalo,_Tbilisi.jpg',
+        anchor: new google.maps.Point(17, 54),
+        scaledSize: new google.maps.Size(35, 35),
+      },
+
+      {
+        url: '/img/Vake_District,_Tbilisi,_Georgia.jpg',
+        anchor: new google.maps.Point(17, 54),
+        scaledSize: new google.maps.Size(35, 35),
+      },
+
+    ]
+
     function addMarker(property){
+
       const marker = new google.maps.Marker({
         position: property.location,
-        map: map
+        map: map,
+        icon: svgIcon
       });
 
       const detailWindow= new google.maps.InfoWindow({
         content: property.content
       });
 
-      marker.addListener('mouseover',()=>{
-        detailWindow.open(map,marker);
+      marker.addListener('click',()=>{
+        boolean=!boolean;
+        if(boolean){
+          detailWindow.open(map,marker);
+          if(property.nextIcons){
+            marker.setIcon(property.nextIcons);
+          }
+         
+        } else{
+          detailWindow.close();
+          if(property.nextIcons){
+            marker.setIcon(svgIcon)
+          }
+        
+        }
+        
       })
+
 
     }
 
 
     addMarker({location: {lat: 41.7151, lng: 44.8271}, content: `<h2>Tbilisi City</h2>`});
-    addMarker({location: {lat: 41.7203, lng: 44.7406}, content: `<h2>Tbilisi Saburtalo</h2>`});
-    addMarker({location: {lat: 41.7096, lng: 44.7569}, content: `<h2>Tbilisi Vake</h2>`});
+    addMarker({location: {lat: 41.7203, lng: 44.7406}, content: `<h5>Saburtalo</h5><p>saburtalo</p><p>Tbilisi Georgia</p>`, nextIcons: nextIcons[0] });
+    addMarker({location: {lat: 41.7096, lng: 44.7569}, content: `<h5>Tbilisi Vake</h5> <p>Vake</p> <p>Tbilisi georgia</p>`, nextIcons: nextIcons[1]});
 
 
 }
